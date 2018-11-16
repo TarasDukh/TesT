4235c1ea63da143da8b2dc514c08c1cef5564226
Taras Dukh <taras.dukh13@gmail.com>
2018-11-16 09:11:27 +0200

    Add lab №3 on c (Matrix).

---
+++ Lab_3_CI_Dukh.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>  
+#include "pch.h"
+#define n 5
+
+
+int my_array[5][5];
+int start_array[] =
+{ 40, 72, 6, 92, 98,
+ 18, -33, -48, 81, 26,
+  1, -4, 6, -2, 0,
+ 36, 9, 0, 4, 1,
+-55, 2, 66, 70, -3
+};
+
+
