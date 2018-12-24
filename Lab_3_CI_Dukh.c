d18a6db853fefcfd03d9464973f63c0d814b1ccf
Taras Dukh <taras.dukh13@gmail.com>
2018-12-24 13:06:19 +0200

    Clean code.

--- Lab_3_CI_Dukh.c
+++ Lab_3_CI_Dukh.c
@@ -1,19 +1,50 @@
-// My comment
-
-#include <stdio.h>
-#include <string.h>
-#include <math.h>  
-#include "pch.h"
+#include <iostream>
 #define n 5
+using namespace std;
 
+// прототип функциї
+void bubbleSort(int arrForSort[]); 
 
-int my_array[5][5];
-int start_array[] =
-{ 40, 72, 6, 92, 98,
- 18, -33, -48, 81, 26,
-  1, -4, 6, -2, 0,
- 36, 9, 0, 4, 1,
--55, 2, 66, 70, -3
-};
+void main()
+{	
+	int arr[n];
 
+	cout << "Початковий масив:\n";
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] =  n - i; // заповнюємо значення по спаданню
+		cout << arr[i] << "\n__\n";
+	}
+	cout << "\n\n";
 
+	bubbleSort(arr);  // передаємо в функцію для сортування
+
+	cout << "Масив після сортування:\n";
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\n__\n";
+	}
+	cout << "\n\n";
+}
+
+void bubbleSort(int arrForSort[])
+{
+	int buff = 0; // для тимчасового збереження, при перезаписі
+
+	for (int i = 0; i < n - 1; i++) // 
+	{
+		// вложенний цикл проходить від четвертого елемента
+		// до першого, знаходимо за допомогою if найлегше значення,
+		// порівнюються зєднані пари елементів,
+		// і підносить його в нулову комірку.
+		for (int j = n - 1; j > i; j--) 
+		{
+			if (arrForSort[j] < arrForSort[j - 1])
+			{
+				buff = arrForSort[j - 1];
+				arrForSort[j - 1] = arrForSort[j];
+				arrForSort[j] = buff;
+			}
+		}
+	}
+}
